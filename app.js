b3d42c71cb992bc1b852f86a1efb390ca0ab8585
Vishal Singh <vishalsinghpanwar02@gmail.com>
2023-08-17 11:19:53 +0530

    css

--- app.js
+++ app.js
@@ -10,16 +10,19 @@ for(let i = 0; i < squares; i++){
   inputElement.setAttribute('type', 'number');
   inputElement.setAttribute('min', '1');
   inputElement.setAttribute('max', 9);
-  if(i < 27){
-      inputElement.classList.add('kesariya');
-  }
-  if(((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53))){
-      inputElement.classList.add('ashok-chakra');
-  }  
-  if(i > 53){
-      inputElement.classList.add('harabhara');
-  }
-
+ 
+  if (
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i < 21) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i < 27) ||
+    ((i % 9 == 3 || i % 9 == 4 || i % 9 == 5) && (i > 27 && i < 53)) ||
+    ((i % 9 == 0 || i % 9 == 1 || i % 9 == 2) && i > 53) ||
+    ((i % 9 == 6 || i % 9 == 7 || i % 9 == 8) && i > 53)){
+      inputElement.classList.add('neelu');
+  } 
+  else {
+     inputElement.classList.add('peelu');
+  } 
+  
   puzzleBoard.appendChild(inputElement);
 }
 
